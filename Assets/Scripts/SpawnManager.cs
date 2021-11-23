a48913e66027409fa498b7458011076cd67cb8ab
Layton4 <Javierete4774@gmail.com>
2021-11-23 19:51:38 +0100

    arreglo

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-
-    public Vector3 spawnPos = new Vector3(0, 0, 30);
-
     private Vector3 spawnPos = new Vector3(0, 0, 30);
-
     private float TimeFirstSpawn = 0.5f;
     private float TimeSpawn = 5f;
 
@@ -28,6 +24,7 @@ public class SpawnManager : MonoBehaviour
 
         float randomY = Random.Range(-10f, 10f); //generamos un número random entre -10 y 10 para la coordenada y
         spawnPos = new Vector3(0, randomY, spawnPos.z + 50f); //el nuevo obstáculo se generará a 50 metros del anterior y a una altura aleatoria
+
         Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation); //generar el prefab, en las coordenadas spawnPos y con la rotación que ya tiene el prefab
     }
 }
